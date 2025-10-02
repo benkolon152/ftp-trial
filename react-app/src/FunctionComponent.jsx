49cb8473d1a7505834614a57786dcfa9c0eeb3c7
Benkolon152 <v.benasz@gmail.com>
2025-10-02 10:18:36 +0200

    Implement ComponentDidMount

--- react-app/src/FunctionComponent.jsx
+++ react-app/src/FunctionComponent.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function FunctionComponent(props){
     const [state, setState] = useState(({}))
 
-    const componentDidMount = () => console.log('componentDidMOunt') 
+    const componentDidMount = () => {
+        console.log('componentDidMOunt')
+        setState({number: 0})
+    } 
     const componentDidUnmount = () => console.log('componentDidUnmount')
     const componentDidUpdate = (prevProps, prevState) => {
         console.log('componentDidUpdate props', prevProps, props)
@@ -21,10 +24,17 @@ export default function FunctionComponent(props){
             console.warn(e)
         }
     }
+
+    const handleIncClick = () => setState(prevState => ({...prevState, number: prevState.number+1}))
+
+    useEffect(()=>{
+        componentDidMount()
+    }, [])
+
     return(
         <div className="card">
-            <p><button>Increase Number</button></p>
-            <p>number: {0}</p>
+            <p><button onClick={handleIncClick}>Increase Number</button></p>
+            <p>number: {state.number}</p>
             <p><button onClick={handleLogClick}> Log props, state</button></p>
         </div>
     )
